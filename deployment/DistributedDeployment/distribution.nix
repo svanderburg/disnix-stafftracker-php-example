a2c3e9be2b6ace42a7e70e156b308e42b7821698
Sander van der Burg <svanderburg@gmail.com>
2010-08-02 17:43:30 +0000

    Initial commit of the Disnix virtualization extension

--- deployment/DistributedDeployment/distribution.nix
+++ deployment/DistributedDeployment/distribution.nix
@@ -3,7 +3,7 @@
 {
   rooms = [ infrastructure.test2 ];
   staff = [ infrastructure.test2 ];
-  stafftracker = [ infrastructure.test2 ];
+  stafftracker = [ infrastructure.test1 ];
   zipcodes = [ infrastructure.test2 ];
 }
 
