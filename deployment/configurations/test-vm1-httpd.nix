9db8d30bee22f9a6bd9beb235292a31a26feba4c
Sander van der Burg <svanderburg@gmail.com>
2017-10-08 18:53:05 +0000

    Use a more decent PHP configuration

--- deployment/configurations/test-vm1-httpd.nix
+++ deployment/configurations/test-vm1-httpd.nix
@@ -5,23 +5,26 @@
     disnix = {
       enable = true;
     };
-    
+
     openssh = {
       enable = true;
     };
-    
+
     httpd = {
       enable = true;
       documentRoot = "/var/www";
       adminAddr = "admin@localhost";
-      extraModules = [
-        { name = "php7"; path = "${pkgs.php}/modules/libphp7.so"; }
-      ];
+      enablePHP = true;
+      extraConfig = ''
+        DirectoryIndex index.php
+      '';
     };
   };
-  
+
   networking.firewall.allowedTCPPorts = [ 80 ];
-  
+
+  time.timeZone = "UTC";
+
   environment = {
     systemPackages = [
       pkgs.mc
